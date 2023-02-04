f554c8c71acdb4b31f091ad3e2ddc9f8f840fd3a
Kemal Şen <55662927+kemlsen@users.noreply.github.com>
2023-02-04 19:52:26 +0300

    Engin Demiroğ - Yazılım Geliştirme Kampı 7. Gün Ödevi

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemoryDal;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryDal/InMemoryCarDal.cs
@@ -0,0 +1,51 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemoryDal
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { new Car { Id = 1, BrandId = 10, ColorId = 100, ModelYear = 2022, DailyPrice = 3500, Description = "Kaliteli araç" },
+            new Car { Id = 2, BrandId = 11, ColorId = 101, ModelYear = 2001, DailyPrice = 1500, Description = "Dandik araç" },};
+
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var deletedCar = _cars.SingleOrDefault(p => p.Id == car.Id);
+            _cars.Remove(deletedCar);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(p => p.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            var updatedCar = _cars.SingleOrDefault(p => p.Id == car.Id);
+            updatedCar.BrandId = car.BrandId;
+            updatedCar.ColorId = car.ColorId;
+            updatedCar.ModelYear = car.ModelYear;
+            updatedCar.Description = car.Description;
+        }
+    }
+}
