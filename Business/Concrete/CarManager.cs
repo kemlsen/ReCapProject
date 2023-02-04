edf2a05e3e4ebce5aca6fdd0bc6b3097de1fdb2a
Kemal Şen <55662927+kemlsen@users.noreply.github.com>
2023-02-04 23:47:37 +0300

    Engin Demiroğ - Yazılım Geliştirme Kampı 8. Gün Ödevi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -18,26 +18,25 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
+        }
         public void Add(Car car)
         {
-            _carDal.Add(car);
+            if (car.Description.Length > 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
         }
-
         public void Delete(Car car)
         {
             _carDal.Delete(car);
         }
-
-        public List<Car> GetAll()
-        {
-            return _carDal.GetAll();
-        }
-
-        public Car GetById(int id)
-        {
-            return _carDal.GetById(id);
-        }
-
         public void Update(Car car)
         {
             _carDal.Update(car);
--- DataAccess/Concrete/InMemoryDal/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemoryDal/InMemoryCarDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,14 +30,14 @@ namespace DataAccess.Concrete.InMemoryDal
             _cars.Remove(deletedCar);
         }
 
-        public List<Car> GetAll()
+        public Car Get(Expression<Func<Car, bool>> filter)
         {
-            return _cars;
+            throw new NotImplementedException();
         }
 
-        public Car GetById(int id)
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
         {
-            return _cars.SingleOrDefault(p => p.Id == id);
+            throw new NotImplementedException();
         }
 
         public void Update(Car car)
